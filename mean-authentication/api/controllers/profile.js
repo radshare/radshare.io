b694015ce44bd19d99ac97f49fb7bed6191d3ce7
RenatoSerk <brickworked@gmail.com>
2020-10-03 16:27:52 -0400

    Make profile pages private
    Profile pages need a jwt token to view

--- mean-authentication/api/controllers/profile.js
+++ mean-authentication/api/controllers/profile.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
 module.exports.profileRead = (req, res) => {
-    console.log(`Reading profile ID: ${req.params.userid}`);
-    res.status(200);
-    res.json({
-        message : `Profile read: ${req.params.userid}`
-    });
+    // If no user ID exists in the JWT return a 401
+    if (!req.payload._id) {
+        res.status(401).json({
+            message: 'UnauthorizedError: private profile'
+        });
+    } else {
+        // Otherwise continue
+        User.findById(req.payload._id).exec(function(err, user) {
+            res.status(200).json(user);
+        });
+    }
 };
--- mean-authentication/app.js
+++ mean-authentication/app.js
@@ -1,9 +1,11 @@
 require('./api/models/db');
+require('./api/config/passport');
 
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
+const passport = require('passport');
 const path = require('path');
 
 const routesApi = require('./api/routes/index');
@@ -14,6 +16,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(passport.initialize());
+app.use("/api", routesApi);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +29,14 @@ app.use('/api', routesApi);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
+});
+
+// Catch unauthorised errors
+app.use((err, req, res) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({ message: `${err.name}: ${err.message}` });
+  }
 });
 
 // error handler
