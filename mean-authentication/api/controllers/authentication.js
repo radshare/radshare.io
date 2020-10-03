8ec65e4fbf2a587b128c9707d75906e8fb26b527
RenatoSerk <brickworked@gmail.com>
2020-10-03 16:27:06 -0400

    Add token sessions
    Added JWT Token sessions

--- mean-authentication/api/controllers/authentication.js
+++ mean-authentication/api/controllers/authentication.js
@@ -1,15 +1,42 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const User = mongoose.model('User');
+
 module.exports.register = (req, res) => {
-    console.log(`Registering user: ${req.body.email}`);
-    res.status(200);
-    res.json({
-        message : `User registered: ${req.body.email}`
+    const user = new User();
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+
+    user.setPassword(req.body.password);
+
+    user.save(() => {
+        const token = user.generateJwt();
+        res.status(200);
+        res.json({
+            token: token
+        });
     });
 };
 
 module.exports.login = (req, res) => {
-    console.log(`Logging in user: ${req.body.email}`);
-    res.status(200);
-    res.json({
-        message : `User logged in: ${req.body.email}`
-    });
+    passport.authenticate('local', (err, user, info) => {
+        // If Passport throws/catches an error
+        if (err) {
+            res.status(404).json(err);
+            return;
+        }
+
+        // If a user is found
+        if (user) {
+            const token = user.generateJwt();
+            res.status(200);
+            res.json({
+                token: token
+            });
+        } else {
+            // If user is not found
+            res.status(401).json(info);
+        }
+    })(req, res);
 };
--- mean-authentication/api/routes/index.js
+++ mean-authentication/api/routes/index.js
@@ -3,9 +3,15 @@ const ctrlProfile = require('../controllers/profile');
 
 const express = require('express');
 const router = express.Router();
+const jwt = require('express-jwt');
+
+const auth = jwt({
+    secret: 'MY_SECRET', //don't keep!
+    userProperty: 'payload'
+});
 
 // profile
-router.get('/profile/:userid', ctrlProfile.profileRead);
+router.get('/profile', auth, ctrlProfile.profileRead);
 
 // authentication
 router.post('/register', ctrlAuth.register);
