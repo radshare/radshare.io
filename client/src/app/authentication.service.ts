4ccef689c4dae659573edbdb87509f7c574416b9
RenatoSerk <brickworked@gmail.com>
2020-10-22 15:04:01 -0400

    Add methods to make API calls
    Added API methods to the authentication service to make post and get calls

--- client/src/app/authentication.service.ts
+++ client/src/app/authentication.service.ts
@@ -47,4 +47,63 @@ export class AuthenticationService {
     window.localStorage.removeItem("mean-token");
     this.router.navigateByUrl("/");
   }
+
+  public getUserDetails(): UserDetails {
+    const token = this.getToken();
+    let payload;
+    if (token) {
+      payload = token.split(".")[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload);
+    } else {
+      return null;
+    }
+  }
+
+  public isLoggedIn(): boolean {
+    const user = this.getUserDetails();
+    if (user) {
+      return user.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+
+  private request(
+    method: "post" | "get",
+    type: "login" | "register" | "profile",
+    user?: TokenPayload
+  ): Observable<any> {
+    let base$;
+
+    if (method === "post") {
+      base$ = this.http.post(`/api/${type}`, user);
+    } else {
+      base$ = this.http.get(`/api/${type}`, {
+        headers: { Authorization: `Bearer ${this.getToken()}` }
+      });
+    }
+
+    const request = base$.pipe(
+      map((data: TokenResponse) => {
+        if (data.token) {
+          this.saveToken(data.token);
+        }
+        return data;
+      })
+    );
+    return request;
+  }
+
+  public register(user: TokenPayload): Observable<any> {
+    return this.request("post", "register", user);
+  }
+
+  public login(user: TokenPayload): Observable<any> {
+    return this.request("post", "login", user);
+  }
+
+  public profile(): Observable<any> {
+    return this.request("get", "profile");
+  }
 }
