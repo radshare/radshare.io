53b72e3173eea5f9af597833543655bc750755ab
RenatoSerk <brickworked@gmail.com>
2020-10-22 15:18:21 -0400

    Generate register, login, home and profile components
    Use Angular CLI to generate components for accounts and profiles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { ClientComponent } from './client/client.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ProfileComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
