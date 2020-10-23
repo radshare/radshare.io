d7ad40e334f247c1f0f11c389a6cc9899700a514
RenatoSerk <brickworked@gmail.com>
2020-10-23 15:30:58 -0400

    Add Angular Auth-Guard
    Generated auth guard service to protect profile pages and updated the profile page to display info

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {RegisterComponent} from "./register/register.component";
+import {AuthGuard} from "./auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService, UserDetails } from "../authentication.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
+  details: UserDetails;
 
-  constructor() { }
+  constructor(private auth: AuthenticationService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.auth.profile().subscribe(
+      user => {
+        this.details = user;
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
-
 }
