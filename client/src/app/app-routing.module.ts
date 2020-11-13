9432cca9f33ec6b9682feb537d9c9e4947623e61
RenatoSerk <brickworked@gmail.com>
2020-11-13 13:26:16 -0500

    Added nonAuthGuard
    Login and register pages are now protected from users who are currently logged in

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import {LoginComponent} from "./login/login.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {RegisterComponent} from "./register/register.component";
 import {AuthGuard} from "./auth.guard";
+import {NonAuthGuard} from "./nonAuth.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent, canActivate: [NonAuthGuard] },
+  { path: "register", component: RegisterComponent, canActivate: [NonAuthGuard] },
   { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] }
 ];
 
