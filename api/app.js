6ab1481c3f4f29b4e056281b7cffce97739837b7
RenatoSerk <brickworked@gmail.com>
2020-12-04 23:01:12 -0500

    Remove unused pug references

--- api/app.js
+++ api/app.js
@@ -12,10 +12,6 @@ const routesApi = require('./routes');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
