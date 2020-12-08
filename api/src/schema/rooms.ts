4c30044acb9207c7494497e5ddd0957a2e376fa8
Benjamin <benjaminpalko@hotmail.com>
2020-12-07 23:04:10 -0500

    Schema files migrated

---
+++ api/src/schema/rooms.ts
@@ -0,0 +1,52 @@
+import {Schema, Document} from "mongoose";
+
+export interface IRoom extends Document {
+	relic: string,
+	quality: string,
+	platform: string,
+	creationDate: Date,
+	expirationDate: number,
+	tenno: string,
+	roomCode: string
+}
+
+export class RoomSchema extends Schema<IRoom> {
+
+	constructor() {
+		super({
+			relic: {
+				type: String,
+				required: true,
+			},
+			quality: {
+				type: String,
+				enum: ['intact', 'exceptional', 'flawless', 'radiant'],
+				required: true
+			},
+			platform: {
+				type: String,
+				enum: ['pc', 'playstation', 'xbox', 'switch'],
+				required: true
+			},
+			creationDate: {
+				type: Date,
+				default: Date.now,
+				expires: 7200,
+				required: true
+			},
+			expirationDate: {
+				type: Number,
+				required: true
+			},
+			tenno: {
+				type: String,
+				required: true
+			},
+			roomCode: {
+				type: String,
+				required: true,
+				unique: true
+			}
+		});
+	}
+}
