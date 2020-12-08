76572dbd48609b6692e663df3d9de3ce792853c8
Benjamin <benjaminpalko@hotmail.com>
2020-12-08 10:14:12 -0500

    home controller

---
+++ api/src/controllers/homeTable-controller.ts
@@ -0,0 +1,47 @@
+import {Connection, Model} from "mongoose";
+import {IRoom, Models} from "../schema";
+
+import {v4 as uuid} from "uuid";
+
+export class HomeTableController {
+
+	private readonly room: Model<IRoom>;
+
+	constructor(connection: Connection) {
+		this.room = connection.model<IRoom>(Models.ROOM);
+	}
+
+	public async post(req, res): Promise<void> {
+		const room = new this.room();
+		room.relic = req.body.relic;
+		room.quality = req.body.quality;
+		room.platform = req.body.platform;
+		room.expirationDate = Date.now() + 7200000;
+		room.tenno = req.body.tenno;
+		room.roomCode = uuid();
+
+		try {
+			let result: IRoom = await room.save();
+			res.status(200);
+			res.json({
+				data : result
+			});
+		} catch (e) {
+			console.error('Error saving room', e);
+			res.status(500);
+		}
+	}
+
+	public async get(req, res): Promise<void> {
+		try {
+			let result: IRoom[] = await this.room.find().exec();
+			res.status(200);
+			res.json({
+				data : result
+			});
+		} catch (e) {
+			console.error('Error retrieving room list', e);
+			res.status(500);
+		}
+	}
+}
